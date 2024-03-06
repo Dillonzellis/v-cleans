c93e91232b94b3fc2dbe157e276d85bb31f6f131
dillonzellis <dillonzellis@gmail.com>
2024-03-06 11:32:21 -0500

    feat: add basic toast for quote

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,18 +1,31 @@
 "use client";
 
 import { sendEmail, confirmationEmail } from "@/actions/sendEmail";
+import { toast } from "sonner";
+
+const test = () => {
+  console.log("test");
+};
 
 export const ContactForm = () => {
   return (
     <form
-      action={async (formData) => {
-        await sendEmail(formData);
-        await confirmationEmail(formData);
-      }}
+      // action={async (formData) => {
+      //   await sendEmail(formData);
+      //   await confirmationEmail(formData);
+      // }}
+      action={test}
     >
       <input name="senderEmail" type="senderEmail" placeholder="Your Email" />
       <textarea name="message" placeholder="Your Message" />
-      <button type="submit">Send</button>
+      <button
+        type="submit"
+        onClick={() => {
+          toast("Ayoo big toast");
+        }}
+      >
+        Send
+      </button>
     </form>
   );
 };
